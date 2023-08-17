130501c07e9adb3c86a85f344e7bd4e8bed97f1b
OkanShr <okan.shr@hotmail.com>
2023-08-17 09:23:10 +0200

    CreatedAt

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -47,40 +47,41 @@ function ThreadCard({
         isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
       }`}
     >
-      <div className='flex items-start justify-between'>
-        <div className='flex w-full flex-1 flex-row gap-4'>
-          <div className='flex flex-col items-center'>
-            <Link href={`/profile/${author.id}`} className='relative h-11 w-11'>
+      <div className="flex items-start justify-between">
+        <div className="flex w-full flex-1 flex-row gap-4">
+          <div className="flex flex-col items-center">
+            <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
               <Image
                 src={author.image}
-                alt='user_community_image'
+                alt="user_community_image"
                 fill
-                className='cursor-pointer rounded-full'
+                className="cursor-pointer rounded-full"
               />
             </Link>
 
-            <div className='thread-card_bar' />
+            <div className="thread-card_bar" />
           </div>
 
-          <div className='flex w-full flex-col gap-4'>
-            <Link href={`/profile/${author.id}`} className='w-fit'>
-              <h4 className='cursor-pointer text-base-semibold text-light-1'>
+          <div className="flex w-full flex-col gap-4">
+            <Link href={`/profile/${author.id}`} className="w-fit">
+              <h4 className="cursor-pointer text-base-semibold text-light-1">
                 {author.name}
               </h4>
             </Link>
-            
-            
-            <p className='mt-2 text-base-medium text-light-2'>{content}</p>
-            {threadImage === "" || threadImage === null? (
-              ''
-            ):<Image
-            src={threadImage}
-            alt='thread_image'
-            width={500}
-            height={500}
-          />}
+
+            <p className="mt-2 text-base-medium text-light-2">{content}</p>
+            {threadImage === "" || threadImage === null ? (
+              ""
+            ) : (
+              <Image
+                src={threadImage}
+                alt="thread_image"
+                width={500}
+                height={500}
+              />
+            )}
             <div className={`${isComment && "mb-10"} mt-5 flex flex-col gap-3`}>
-              <div className='flex gap-3.5'>
+              <div className="flex gap-3.5">
                 {/* <Image
                   src='/assets/heart-gray.svg'
                   alt='heart'
@@ -88,14 +89,17 @@ function ThreadCard({
                   height={24}
                   className='cursor-pointer object-contain'
                 /> */}
-                <Link href={`/thread/${id}`}>
+                <Link className="flex flex-col" href={`/thread/${id}`}>
                   <Image
-                    src='/assets/reply.svg'
-                    alt='commenticon'
+                    src="/assets/reply.svg"
+                    alt="commenticon"
                     width={32}
                     height={32}
-                    className='cursor-pointer object-contain'
+                    className="cursor-pointer object-contain"
                   />
+                  <p className="text-light-1">{`${formatDateString(
+                    createdAt
+                  )}`}</p>
                 </Link>
                 {/* <Image
                   src='/assets/repost.svg'
@@ -115,7 +119,7 @@ function ThreadCard({
 
               {isComment && comments.length > 0 && (
                 <Link href={`/thread/${id}`}>
-                  <p className='mt-1 text-subtle-medium text-gray-1'>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
                     {comments.length} repl{comments.length > 1 ? "ies" : "y"}
                   </p>
                 </Link>
@@ -134,7 +138,7 @@ function ThreadCard({
       </div>
 
       {!isComment && comments?.length > 0 && (
-        <div className='ml-1 mt-3 flex items-center gap-2'>
+        <div className="ml-1 mt-3 flex items-center gap-2">
           {comments.slice(0, 2).map((comment, index) => (
             <Image
               key={index}
@@ -147,7 +151,7 @@ function ThreadCard({
           ))}
 
           <Link href={`/thread/${id}`}>
-            <p className='mt-1 text-subtle-medium text-gray-1'>
+            <p className="mt-1 text-subtle-medium text-gray-1">
               {comments.length} repl{comments.length > 1 ? "ies" : "y"}
             </p>
           </Link>
@@ -157,9 +161,9 @@ function ThreadCard({
       {!isComment && community && (
         <Link
           href={`/communities/${community.id}`}
-          className='mt-5 flex items-center'
+          className="mt-5 flex items-center"
         >
-          <p className='text-subtle-medium text-gray-1'>
+          <p className="text-subtle-medium text-gray-1">
             {formatDateString(createdAt)}
             {community && ` - ${community.name} Community`}
           </p>
@@ -169,7 +173,7 @@ function ThreadCard({
             alt={community.name}
             width={14}
             height={14}
-            className='ml-1 rounded-full object-cover'
+            className="ml-1 rounded-full object-cover"
           />
         </Link>
       )}
